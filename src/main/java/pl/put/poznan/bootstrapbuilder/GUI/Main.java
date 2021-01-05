d328e61abcdf5d687acdc99e799bbb0a251834f5
Radziu1202 <bkamil0320@gmail.com>
2021-01-05 15:39:18 +0100

    gui

--- src/main/java/pl/put/poznan/bootstrapbuilder/GUI/Main.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/GUI/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package pl.put.poznan.bootstrapbuilder.GUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Bootstrap builder");
         primaryStage.setScene(new Scene(root, 500, 475));
         primaryStage.show();
